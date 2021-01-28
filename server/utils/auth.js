aa5128e0fed2cd1a1062b7fc4771e08d7e6b9e4d
Garlic Fiend <suzannaakins@gmail.com>
2021-01-28 13:52:52 -0800

    added .env folder to the server added .gitignore to server to ignore .env changed the secret from the example

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
+require("dotenv").config();
 
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const secret = process.env.SECRET;
+const expiration = process.env.EXPIRATION;
 
 module.exports = {
   signToken: function({ username, email, _id }) {
