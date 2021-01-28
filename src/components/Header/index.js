b58295203b6b469609819a5fb37f6c4ddad079a5
Chris Meissner <chrismeissner@Chriss-MBP.lan>
2021-01-27 20:40:11 -0800

    Login Component & Styling

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function Header() {
+  return (
+    <div className="HeaderContainer">
+      <h1 className="HeaderTitle" href="/">
+        Petter Life
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
