0e38518e329213a633abb7a0f0ffacf1afc8cd86
ChrisMeissner <69017427+ChrisMeissner@users.noreply.github.com>
2021-01-28 19:17:36 -0800

    Feature/login submit (#10)
    * fix Number in type defs to be int instead
    
    * change href links to signin/login pages to Link components
    
    * change both hrefs to Link Components and  alter param info in mutation.js
    
    Co-authored-by: Chris Meissner <chrismeissner@Chriss-MBP.lan>
    Co-authored-by: Suzanna Akins <68711381+suzannaakins@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,35 @@ import './App.css';
 import Homepage from './pages/Homepage';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token')
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql',
+})
+
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={ Homepage } />
-        </Switch>
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Homepage} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route component={NoMatch} />
+            </Switch>
+          </StoreProvider>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
