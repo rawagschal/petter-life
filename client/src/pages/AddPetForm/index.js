28a8c3d89429ac7a24893a8d8a4ed9c445118b4a
Garlic Fiend <suzannaakins@gmail.com>
2021-01-31 15:22:50 -0800

    handleFormSubmit and handleChange functions added to AddPetForm page

--- client/src/pages/AddPetForm/index.js
+++ client/src/pages/AddPetForm/index.js
@@ -1,45 +1,79 @@
-import  React, { useState } from "react";
+import React, { useState } from "react";
 import { useMutation } from '@apollo/react-hooks';
 import { ADD_OWNED_PET } from '../../utils/mutations';
 import './index.css';
 
 function AddPetForm() {
-  const [formState, setFormState] = useState({ name: '', email: '', password: '' });
+  const [formState, setFormState] = useState({ petName: '', petType: '', petAge: '', petDescription: '' });
+  const [addOwnedPet, { error }] = useMutation(ADD_OWNED_PET);
 
-  
+  const handleFormSubmit = async event => {
+    event.preventDefault();
+
+    try {
+      const mutationResponse = await addOwnedPet({
+        variables: {
+          petName: formState.petName,
+          petType: formState.petType,
+          petAge: formState.petAge,
+          petGender: formState.petGender,
+          petLocation: formState.petLocation,
+          petDescription: formState.petDescription
+        }
+      })
+
+      const result = mutationResponse.data;
+      console.log(result);
+    }  catch (e) {
+      console.log(e);
+    }
+    
+  };
+
+    const handleChange = event => {
+      const { name, value } = event.target;
+      setFormState({
+        ...formState,
+        [name]: value
+      });
+    };
 
   return (
     <div className="AddPetSection">
       <div className="AddPetContainer">
         <div className="AddPetTitle">Fill out this form to create your pet's adoption post</div>
-        <form className="AddPetInformation">
+        <form onSubmit={handleFormSubmit}
+        className="AddPetInformation">
           <div className="AddPetInformationInputContainer">
             <div className="AddPetFormLeft">
               <div className="AddPetName">
-                <label HTMLfor="petName">Name:</label>
-                <input 
-                  className="AddPetNameInputField" 
-                  id="petName" 
-                  name="petName" 
-                  type="input" 
+                <label HTMLfor="petName">Pet Name:</label>
+                <input
+                  className="AddPetNameInputField"
+                  id="petName"
+                  name="petName"
+                  type="input"
+                  onChange={handleChange}
                 />
               </div>
               <div className="AddPetType">
-                <label HTMLfor="petType">Type:</label>
-                <select className="AddPetPetTypeInputField" id="petType" name="petType">
-                  <option value="Dog">Dog</option>
-                  <option value="Cat">Cat</option>
-                  <option value="Bird">Bird</option>
-                  <option value="other">Other</option>
-                </select>
+                <label HTMLfor="petType">Type of Animal:</label>
+                <input
+                  className="AddPetTypeInputField"
+                  id="petType"
+                  name="petType"
+                  type="input"
+                  onChange={handleChange}
+                />
               </div>
               <div className="AddPetAge">
-                <label HTMLfor="petAge">Age:</label>
-                <input 
-                  className="AddPetAgeInputField" 
-                  id="petAge" 
-                  name="petName" 
-                  type="input" 
+                <label HTMLfor="petAge">Pet Age:</label>
+                <input
+                  className="AddPetAgeInputField"
+                  id="petAge"
+                  name="petName"
+                  type="input"
+                  onChange={handleChange}
                 />
               </div>
               <div className="AddPetGender">
@@ -60,23 +94,25 @@ function AddPetForm() {
                 </select>
               </div>
               <div className="AddPetLocation">
-                <label HTMLfor="petLocation">Location Zipcode:</label>
-                <input 
-                  className="AddPetLocationInputField" 
-                  id="petLocation" 
-                  name="petLocation" 
-                  type="input" 
+                <label HTMLfor="petLocation">Location (Zipcode):</label>
+                <input
+                  className="AddPetLocationInputField"
+                  id="petLocation"
+                  name="petLocation"
+                  type="input"
+                  onChange={handleChange}
                 />
               </div>
               <div className="AddPetDescription">
                 <label HTMLfor="petDescription">Description:</label>
-                <textarea 
-                  className="AddPetDescriptionInputField" 
-                  name="petDescription" 
+                <textarea
+                  className="AddPetDescriptionInputField"
+                  name="petDescription"
+                  onChange={handleChange}
                 />
               </div>
-            </div>  
-          </div>        
+            </div>
+          </div>
           <button className="AddPetBtn" type="submit">Add Pet</button>
         </form>
       </div>
