33b80cc2bc45f3714dce033f683e9cd222aae179
Garlic Fiend <suzannaakins@gmail.com>
2021-01-31 12:16:32 -0800

    homepage conditionally renders signup form if user is NOT logged in

--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import Signup from '../Signup'
+import Auth from '../../utils/auth';
 
 
 function Homepage() {
+
+  //conditionally render signup page
+  function showSignup() {
+    if (Auth.loggedIn()) {
+      return (
+        <div>
+          
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Signup />
+        </div>
+      );
+    }
+  }
+
   return (
     <div>
+      {showSignup()}
     </div>
   );
+
 }
 
 export default Homepage;
