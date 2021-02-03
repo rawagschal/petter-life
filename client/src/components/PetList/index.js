4a39bc8336eb3259473e7c96c0182d6102ba05bd
Garlic Fiend <suzannaakins@gmail.com>
2021-02-03 11:57:07 -0800

    like button changes text when clicked to unlike, and vice versa

--- client/src/components/PetList/index.js
+++ client/src/components/PetList/index.js
@@ -2,48 +2,22 @@ import React, { useState } from 'react';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { QUERY_PETS } from "../../utils/queries";
 import './index.css';
-import { ADD_LIKED_PET } from '../../utils/mutations';
+// import { ADD_LIKED_PET } from '../../utils/mutations';
 
 const PetList = ({ pets }) => {
 
     const { loading, data } = useQuery(QUERY_PETS);
     console.log('data', data);
 
-    const [petState, setPetState] = useState({ liked: false });
     // const [addLikedPet] = useMutation(ADD_LIKED_PET);
+
+    const [petState, setPetState] = useState({ liked: false });
 
     const buttonTextHandler = async event => {
         event.preventDefault();
-        console.log("you clicked this button");
-        console.log(petState);
-        const petState = () => {
-            if (petState.liked = false) {
-                return true
-            } else {
-                return false;
-            }
-        }
 
-        const textFlip = (petState) => {
-            if (petState) {
-                return (
-                    <button>
-                        Unlike this Pet
-                    </button>
-                );
-            } else {
-                return (
-                    <button>Like this Pet</button>
-                )
-            }
-        }
-
-        textFlip();
-
-        // const result = await addLikedPet({
-        // });
-
-        // console.log(result);
+        setPetState(!petState)
+        console.log(petState); 
     }
 
 
@@ -66,7 +40,12 @@ const PetList = ({ pets }) => {
                             <li>Fixed: {pet.fixed}</li>
                             <li>{pet.description}</li>
                         </ul>
-                        <button className="LikePetBtn" onClick={buttonTextHandler}>Like this Pet</button>
+
+                        {!petState ? (
+                            <button className="LikePetBtn" onClick={buttonTextHandler}>Unlike this Pet</button>
+                        ) : (
+                            <button className="LikePetBtn" onClick={buttonTextHandler}>Like this Pet</button>
+                        )}
                     </div>
                 ))}
             </div>
