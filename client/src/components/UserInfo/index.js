915f099f53da42b6919701afb92124fb1bed3fb6
Chris Meissner <chrismeissner@Chriss-MacBook-Pro.local>
2021-02-03 15:04:22 -0700

    Add UserInfo component Change flex-direction stylings for AddPetSection Add UserInfo component to Homepage page and add working stylings Data for user is undefined rn. Will create query for me instead of just user and see if that works.

---
+++ client/src/components/UserInfo/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Auth from '../../utils/auth';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_USER } from "../../utils/queries";
+import './index.css';
+
+function UserInfo({ user }) {
+
+  const { loading, data } = useQuery(QUERY_USER);
+  console.log('data', data);
+
+  return (
+    <div className="UserInfoContainer">
+      {!loading && data.user(user => (
+        <div className="UserListing" key={user._id}>
+          <div className="UsernameDisplay">
+              <div>{user.username}</div>
+          </div>
+        </div>
+      ))}
+      <button className="UserInfoProfileBtn">Profile</button>
+      <button className="UserInfoDonateBtn">Donate</button>
+      <button className="UserInfoLogoutBtn"> Logout
+        <a href="/" onClick={() => Auth.logout()}></a>
+      </button>
+    </div>
+  );
+}
+
+export default UserInfo;
