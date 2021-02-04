d23224883791ef413cf5c9d49b91ed2cc9e179a4
Garlic Fiend <suzannaakins@gmail.com>
2021-02-04 15:50:39 -0800

    service worker added but commented out because the register function is not working

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-serviceWorker.register();
+// serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
