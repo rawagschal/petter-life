36516641f9237bc959231f8a34839a9a386d7101
ChrisMeissner <69017427+ChrisMeissner@users.noreply.github.com>
2021-02-04 15:58:52 -0700

    Feature/stripe donation (#31)
    * started feature/stripe-donation
    
    * donation form styling
    
    * added success page front end
    
    * attempting stripe-donation backedn
    
    * react-stripe CardElement not rendering
    
    Co-authored-by: Rachel Wagschal <rawagschal@gmail.com>

---
+++ client/src/pages/DonationSuccess/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function DonationSuccess() {
+    return (
+        <div className="DonationSuccessSection">
+            <div className="DonationSuccessContainer">
+                <div className="DonationSuccessMessage">Thank you for donating!</div>
+            </div>
+        </div>
+    )
+}
+
+export default DonationSuccess;
---
+++ server/stripe.js
@@ -0,0 +1,15 @@
+const testStripe = async event => {
+
+    try {
+        const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: 1477, // $14.77, an easily identifiable amount
+          currency: 'usd',
+        });
+        console.log('Worked! ', paymentIntent.id);
+      } catch(err) {
+        console.log('Error! ', err.message);
+      }
+}
+
+testStripe();
