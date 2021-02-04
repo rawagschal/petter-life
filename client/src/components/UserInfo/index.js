6996c04282cd75364992aac0e439eaee3cae3b92
Kim Mulligan <kimmariemulligan@gmail.com>
2021-02-03 20:11:26 -0800

    set up globalStore using  context, added in logic to log the user in and out of the store, synced state of the store with the jwt in local storage so that the user info is always in the store, wrote the me query and tested and integrated it to fetch the user data into the header component. User name displays on left.

--- client/src/components/UserInfo/index.js
+++ client/src/components/UserInfo/index.js
@@ -3,14 +3,16 @@ import Auth from '../../utils/auth';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_USER } from "../../utils/queries";
 import './index.css';
+import { useStoreContext } from "../../utils/GlobalState";
 
 function UserInfo() {
 
   // const { loading, data } = useQuery(QUERY_USER);
   // console.log('user data', data);
-
+   const { globalStore, dispatch } = useStoreContext()
   return (
     <div className="UserInfoContainer">
+
       {/* {!loading && data.user(user => (
         <div className="UserListing" key={user._id}>
           <div className="UsernameDisplay">
@@ -18,11 +20,10 @@ function UserInfo() {
           </div>
         </div>
       ))} */}
+      <h3>{globalStore.user.username}</h3>
       <button className="UserInfoProfileBtn">Profile</button>
       <button className="UserInfoDonateBtn">Donate</button>
-      <button className="UserInfoLogoutBtn"> Logout
-        <a href="/" onClick={() => Auth.logout()}></a>
-      </button>
+      <button className="UserInfoLogoutBtn" onClick={() => Auth.logout()}>Logout</button>
     </div>
   );
 }
